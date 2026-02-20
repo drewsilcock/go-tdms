aec5e249916c8822c7a1d751e53da546634e21bd
Drew Silcock <drew.silcock@stfc.ac.uk>
2026-02-20 11:34:10 +0000

    Move daqmx_simple_test.go into daqmx_test.go

--- daqmx_test.go
+++ daqmx_test.go
@@ -727,6 +727,115 @@ func TestDAQmxDigitalLineScalerData(t *testing.T) {
 	}
 }
 
+func TestHexToBytes(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected []byte
+	}{
+		{
+			name:     "simple hex",
+			input:    "01 02 03 04",
+			expected: []byte{0x01, 0x02, 0x03, 0x04},
+		},
+		{
+			name:     "hex with newlines",
+			input:    "01 02\n03 04",
+			expected: []byte{0x01, 0x02, 0x03, 0x04},
+		},
+		{
+			name:     "hex with tabs",
+			input:    "01\t02\t03\t04",
+			expected: []byte{0x01, 0x02, 0x03, 0x04},
+		},
+		{
+			name:     "FF bytes",
+			input:    "FF FF FE FF",
+			expected: []byte{0xFF, 0xFF, 0xFE, 0xFF},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := hexToBytes(tt.input)
+			if !bytes.Equal(result, tt.expected) {
+				t.Errorf("hexToBytes(%q) = %v, want %v", tt.input, result, tt.expected)
+			}
+		})
+	}
+}
+
+func TestMetadataStructure(t *testing.T) {
+	root := rootMetadata()
+	if len(root) < 10 {
+		t.Errorf("Root metadata too short: %d bytes", len(root))
+	}
+
+	group := groupMetadata()
+	if len(group) < 15 {
+		t.Errorf("Group metadata too short: %d bytes", len(group))
+	}
+
+	scaler := daqmxScalerMetadata(0, 3, 0, 0)
+	if len(scaler) != 20 {
+		t.Errorf("DAQmx format-changing scaler should be 20 bytes, got %d", len(scaler))
+	}
+
+	digitalScaler := digitalScalerMetadata(0, 0, 0, 0)
+	if len(digitalScaler) != 17 {
+		t.Errorf("DAQmx digital line scaler should be 17 bytes, got %d", len(digitalScaler))
+	}
+}
+
+func TestGeneratedFileStructure(t *testing.T) {
+	scalerMeta := daqmxScalerMetadata(0, 3, 0, 0)
+
+	metadata := buildMetadata(3,
+		rootMetadata(),
+		groupMetadata(),
+		daqmxChannelMetadata("Channel1", 4, []uint32{2}, [][]byte{scalerMeta}, 0xFFFFFFFF, nil),
+	)
+
+	rawData := hexToBytes("01 00 02 00 FF FF FE FF")
+
+	tf := newTestFile()
+	tf.addSegment(segmentTOC(), metadata, rawData)
+
+	fileBytes := tf.build()
+
+	if !bytes.HasPrefix(fileBytes, tdmsMagicBytes) {
+		t.Error("File doesn't start with TDSm magic bytes")
+	}
+
+	if string(fileBytes[0:4]) != "TDSm" {
+		t.Errorf("Invalid magic bytes: %v", fileBytes[0:4])
+	}
+
+	expectedSize := 28 + len(metadata) + len(rawData)
+	if len(fileBytes) != expectedSize {
+		t.Errorf("Expected file size %d, got %d", expectedSize, len(fileBytes))
+	}
+}
+
+func TestReadRealDAQmxFile(t *testing.T) {
+	file, err := Open("testdata/POC_MultisamplingRate.tdms")
+	if err != nil {
+		t.Fatalf("Failed to open test file: %v", err)
+	}
+	defer file.Close() // nolint:errcheck // test file cleanup
+
+	for groupName, group := range file.Groups {
+		for channelName, channel := range group.Channels {
+			if channel.NumValues() > 0 {
+				_, err := channel.ReadAll()
+				if err != nil {
+					t.Errorf("Failed to read channel '%s'/'%s': %v", groupName, channelName, err)
+				}
+			}
+		}
+	}
+}
+
 func TestDAQmxDataTypeEnumValues(t *testing.T) {
 	tests := []struct {
 		name     string
